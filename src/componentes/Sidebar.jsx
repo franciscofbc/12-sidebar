c3fa8480cb34be34e23c48cd4ba24fde9ab13d21
Francisco Berti da Cruz <fbc991@live.com>
2023-07-19 16:07:45 -0300

    .

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,8 +1,55 @@
+import { IoClose } from 'react-icons/io5';
 import { useGlobalContext } from '../context';
+import { links, social } from '../data';
+import logo from '../logo.svg';
 
 const Sidebar = () => {
-  const { isSidebarOpen, closSidebar } = useGlobalContext();
-  return <nav>{isSidebarOpen ? 'sidebar' : ''}</nav>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <section>
+      {isSidebarOpen && (
+        <article className="sidebar">
+          <div>
+            <div className="header">
+              <img src={logo} alt="logo" />
+              <button
+                type="button"
+                onClick={closeSidebar}
+                className="btn-close"
+              >
+                <IoClose />
+              </button>
+            </div>
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text, icon } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>
+                      {icon}
+                      {text}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <ul className="social-links">
+            {social.map((socialLink) => {
+              const { id, url, icon } = socialLink;
+              return (
+                <li key={id}>
+                  <a href={url} target="_blank" rel="noopener noreferrer">
+                    {icon}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </article>
+      )}
+    </section>
+  );
 };
 
 export default Sidebar;
