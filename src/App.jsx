0100b96aaf037fa6607a0204709495e82e6663bc
Francisco Berti da Cruz <fbc991@live.com>
2023-07-19 22:49:33 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <main>
       <Home />
-      <Modal />
       <Sidebar />
+      <Modal />
     </main>
   );
 };
--- src/componentes/Modal.jsx
+++ src/componentes/Modal.jsx
@@ -1,9 +1,24 @@
 import { useGlobalContext } from '../context';
+import { IoClose } from 'react-icons/io5';
 
 const Modal = () => {
   const { isModalOpen, closeModal } = useGlobalContext();
 
-  return <article>{isModalOpen ? 'modal' : ''}</article>;
+  return (
+    <section>
+      {isModalOpen && (
+        <>
+          <article className="modal">
+            <button type="button" onClick={closeModal} className="btn-close">
+              <IoClose />
+            </button>
+            <h4>modal content</h4>
+          </article>
+          <article className="overlay"></article>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default Modal;
